9c7619fe8ba6754494f79124cc170a96ccbfcd84
Iris Wang <100994372+Iris06-cs@users.noreply.github.com>
2023-11-08 18:14:59 -0800

    Time: 45 ms (74.54%), Space: 17.2 MB (71.53%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -1,14 +1,28 @@
+# class Solution:
+#     def fizzBuzz(self, n: int) -> List[str]:
+#         answer=[]
+# #         loop from 1 to n
+#         for i in range(1,n+1):
+#             if i % 3 == 0 and i % 5 == 0:
+#                 answer.append("FizzBuzz")
+#             elif i % 3 == 0 and i % 5 != 0:
+#                 answer.append("Fizz")
+#             elif i % 5 == 0 and i % 3 != 0:
+#                 answer.append("Buzz")
+#             else:
+#                 answer.append(str(i))
+#         return answer
+
 class Solution:
-    def fizzBuzz(self, n: int) -> List[str]:
+    def fizzBuzz(self,n: int) -> List[str]:
         answer=[]
-#         loop from 1 to n
-        for i in range(1,n+1):
-            if i % 3 == 0 and i % 5 == 0:
-                answer.append("FizzBuzz")
-            elif i % 3 == 0 and i % 5 != 0:
-                answer.append("Fizz")
-            elif i % 5 == 0 and i % 3 != 0:
-                answer.append("Buzz")
-            else:
-                answer.append(str(i))
+        for i in range(1, n+1):
+            ans=""
+            if i % 3 == 0:
+                ans+="Fizz"
+            if i % 5 == 0:
+                ans+="Buzz"
+            if not ans:
+                ans=str(i)
+            answer.append(ans)
         return answer
