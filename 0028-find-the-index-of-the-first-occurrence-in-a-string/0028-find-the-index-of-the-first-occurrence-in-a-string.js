645f6f5e40a2e9f2257619b880b6bd6f42868f8c
Iris Wang <100994372+Iris06-cs@users.noreply.github.com>
2023-11-18 20:35:45 -0800

    Time: 86 ms (5.16%), Space: 45.8 MB (5.25%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,6 +4,27 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    return haystack.indexOf(needle)
+//     O(N)
+    // return haystack.indexOf(needle)
     
+// check if needle is sub-string of haystack
+// edge case, needle is longer than haystack=> cannot be sub-string return -1
+// same length, check if needle and haystack is same string
+// otherwise, get the length of needle
+//     loop thru haystack to get each sub-stirng with same length as needle, and compare
+
+    if(haystack.length<needle.length) return -1
+    if(haystack.length===needle.length){
+        if(haystack===needle) return 0
+        return -1
+    }
+    const needleLength=needle.length
+    for(i=0;i<haystack.length;i++){
+        const subString=haystack.slice(i,i+needleLength)
+        console.log(subString)
+        if(subString===needle) return i
+    
+    }
+    return -1
+       
 };
