79bb9d9eef5e7cef44069e1df6f158736c8b70c1
Iris Wang <100994372+Iris06-cs@users.noreply.github.com>
2023-11-19 21:52:37 -0800

    Time: 80 ms (42.23%), Space: 17.7 MB (9.50%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -6,18 +6,31 @@ class Solution:
         # if so return the two index otherwise return []
         # try to complete in better time complexity than O(n^2)
 #         O(n^2)
-        res=[]
-        for i,num in enumerate(nums):
-            diff=target-num
-            rest_nums=nums[i+1:len(nums)]
-            # print(rest_nums,"line13")
-            for j,num_rest in enumerate(rest_nums):
-                if diff==num_rest:
-                    res.append(i)
-                    res.append(i+j+1)
-                    return res
+#         res=[]
+#         for i,num in enumerate(nums):
+#             diff=target-num
+#             rest_nums=nums[i+1:len(nums)]
+       
+#             for j,num_rest in enumerate(rest_nums):
+#                 if diff==num_rest:
+#                     res.append(i)
+#                     res.append(i+j+1)
+#                     return res
                 
-        return res
+#         return res
+        # better solution
+
+
+        tracker={}
+        for idx, num in enumerate(nums):
+            diff=target-num
+            if diff in tracker:
+                return [tracker[diff],idx]
+            tracker[num]=idx
+             
+        return []
+                
+
                 
             
 
