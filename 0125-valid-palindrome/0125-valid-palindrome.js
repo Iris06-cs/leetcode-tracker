fcb19232e0d7c90eec0f00c4fb05b285e8cf7e99
Iris Wang <100994372+Iris06-cs@users.noreply.github.com>
2023-11-20 23:39:49 -0800

    Time: 65 ms (49.27%), Space: 44.1 MB (87.67%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -4,16 +4,20 @@
  */
 var isPalindrome = function(s) {
     let lowerS=s.toLowerCase()
-    let trimedS=""
-    const alphaChar="abcdefghijklmnopqrstuvwxyz0123456789"
-    for( let char of lowerS){
-        if(alphaChar.includes(char)) trimedS+=char
-    }
-    // console.log(trimedS)
+    // let trimedS=""
+    // const alphaChar="abcdefghijklmnopqrstuvwxyz0123456789"
+    let trimedS=lowerS.replace(/[^a-z0-9]/g,"")
+    
     if(! trimedS.length) return true
+    
+    // for( let char of lowerS){
+    //     if(alphaChar.includes(char)) trimedS+=char
+    // }
+    
     let i=0
     let j=trimedS.length-1
-    while(i<trimedS.length && j>=0){
+    // while(i<trimedS.length && j>=0){
+    while(i<=j){
         // console.log(trimedS[i],trimedS[j])
         if(trimedS[i]!==trimedS[j]) return false
         i++
