077813860e86414300d2580b1840512e07095275
Iris Wang <100994372+Iris06-cs@users.noreply.github.com>
2023-11-24 15:48:26 -0800

    Time: 70 ms (64.39%), Space: 45.9 MB (30.48%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.js
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.js
@@ -7,11 +7,19 @@ var winnerOfGame = function(colors) {
 //     look for aaa bbb pattern substr
     let AliceScore=0
     let BobScore=0
-    for(let i=0;i<colors.length-2;i++){
-        let subString=colors.slice(i,i+3)
-        // console.log(subString)
-        if(subString==="AAA") AliceScore++
-        else if(subString==="BBB") BobScore++
+//     length-2 not length -3
+    // for(let i=0;i<colors.length-2;i++){//O(n)
+    //     let subString=colors.slice(i,i+3)// O(3)
+    //     // console.log(subString)
+    //     if(subString==="AAA") AliceScore++
+    //     else if(subString==="BBB") BobScore++
+    // }
+    
+     for (let i = 0; i < colors.length - 2; i++) {
+        if (colors[i] === colors[i+1] && colors[i+1] === colors[i+2]) {
+            if (colors[i] === 'A') AliceScore++;
+            else if (colors[i] === 'B') BobScore++;
+        }
     }
     // console.log(AliceScore,BobScore)
     return AliceScore>BobScore
