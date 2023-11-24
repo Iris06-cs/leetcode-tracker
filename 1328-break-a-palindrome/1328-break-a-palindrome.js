3c92ece3cab076be5660170ba30905aa6fccaf6b
Iris Wang <100994372+Iris06-cs@users.noreply.github.com>
2023-11-23 22:34:28 -0800

    Time: 49 ms (62.92%), Space: 41.8 MB (74.16%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.js
+++ 1328-break-a-palindrome/1328-break-a-palindrome.js
@@ -5,21 +5,30 @@
 var breakPalindrome = function(palindrome) {
     if(palindrome.length<=1) return ""
     
-    let subString=palindrome.slice(0,Math.floor(palindrome.length/2)).split("")
-     let lastHalfStr=palindrome.slice(Math.floor(palindrome.length/2))
+//     let subString=palindrome.slice(0,Math.floor(palindrome.length/2)).split("")
+//      let lastHalfStr=palindrome.slice(Math.floor(palindrome.length/2))
 
-//     if any char is not "a" change it to a
-//     if all chars are "a" change the last char to"b"
-    for(let i=0;i<subString.length;i++){
-        let char=subString[i]
-        if(char !=="a") {
+// //     if any char is not "a" change it to a
+// //     if all chars are "a" change the last char to"b"
+//     for(let i=0;i<subString.length;i++){
+//         let char=subString[i]
+//         if(char !=="a") {
 
-            subString[i]="a"
-            return subString.join("")+lastHalfStr
+//             subString[i]="a"
+//             return subString.join("")+lastHalfStr
+//         }
+//     }    
+//    let res= palindrome.split("")
+//    res[res.length-1]="b"
+//     res=res.join("")
+//     return     res
+    
+     for (let i = 0; i < Math.floor(palindrome.length / 2); i++) {
+        if (palindrome[i] !== 'a') {
+            return palindrome.substring(0, i) + 'a' + palindrome.substring(i + 1);
         }
-    }    
-   let res= palindrome.split("")
-   res[res.length-1]="b"
-    res=res.join("")
-    return     res
+    }
+
+    // All characters in the first half are 'a', change the last character to 'b'
+    return palindrome.substring(0, palindrome.length - 1) + 'b';
 };
