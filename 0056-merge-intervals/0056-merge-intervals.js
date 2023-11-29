be4b204d947cb23916b0c0dcbc3040b5502d468c
Iris Wang <100994372+Iris06-cs@users.noreply.github.com>
2023-11-28 23:48:22 -0800

    Time: 131 ms (9.06%), Space: 52 MB (9.77%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+// sort intervals based on start value
+    intervals.sort((a,b)=>a[0]-b[0])//O(nlogn)
+    console.log(intervals)
+    res=[intervals[0]]
+    
+    for(let i=1;i<intervals.length;i++){
+        let prevEnd=res[res.length-1][1]
+        
+        let start=intervals[i][0]
+        let end=intervals[i][1]
+        
+//         is overlap
+        if(start<=prevEnd) res[res.length-1][1]=Math.max(prevEnd,end) 
+        else res.push(intervals[i])
+        
+    }
+    return res
+};
