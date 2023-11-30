ad44599e1741f396079e13d485d9274dfcc68baa
Iris Wang <100994372+Iris06-cs@users.noreply.github.com>
2023-11-30 00:29:13 -0800

    Time: 54 ms (65.41%), Space: 42.4 MB (52.16%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -2,23 +2,51 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
+// var isValid = function(s) {
  
-        let stack=[]
-        let opening=["(","{","["]
-        let closing={")":"(",
+//         let stack=[]
+//         let opening=["(","{","["]
+//         let closing={")":"(",
+//                      "}":"{",
+//                      "]":"["}
+//         for(let char of s){
+//            if(opening.indexOf(char)>=0) stack.push(char)
+//             else{
+//                 if(closing[char]&&closing[char]===stack[stack.length-1]) stack.pop()
+//                 else return false
+//             }
+        
+        
+//     }
+//     if(stack.length>0) return false
+//     return true
+    
+// };
+
+var isValid = function(s) {
+// use stack to keep track of openning parentheses
+// iterate through the string, if it's an openting parenthese, push it to the stack
+    // if char is a closing parenthese, compare with the last element in stack
+    // -if match with the last open parenthese, pop it off the stack and continue
+
+    let stack=[]
+    let openingParentheses="([{"
+    let closingParentheses={")":"(",
                      "}":"{",
                      "]":"["}
-        for(let char of s){
-           if(opening.indexOf(char)>=0) stack.push(char)
-            else{
-                if(closing[char]&&closing[char]===stack[stack.length-1]) stack.pop()
-                else return false
-            }
-        
-        
+    
+    for(let char of s){
+        if(openingParentheses.includes(char)) stack.push(char)
+        else{
+            if(closingParentheses[char] && closingParentheses[char]===stack[stack.length-1]) stack.pop()
+            else return false
+          
+        }
     }
-    if(stack.length>0) return false
+   if(stack.length>0) return false
     return true
+    
+
+  
     
 };
