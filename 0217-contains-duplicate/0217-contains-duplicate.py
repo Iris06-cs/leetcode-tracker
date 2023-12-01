43b786efe6a2c582a38870da816c408e3df8aa2b
Iris Wang <100994372+Iris06-cs@users.noreply.github.com>
2023-12-01 11:15:15 -0800

    Time: 480 ms (46.95%), Space: 33.5 MB (11.58%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -22,7 +22,8 @@ class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         seen={}
         for num in nums:
-            if num in seen.keys():
+            # if num in seen.keys():
+            if seen.get(num) is not None:
                 return True
             seen[num]=1
         return False
