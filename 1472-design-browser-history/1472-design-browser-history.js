05a01858e2812b376f6ab8cb125dcfb8fb53c89f
Iris Wang <100994372+Iris06-cs@users.noreply.github.com>
2023-11-30 18:08:47 -0800

    Time: 160 ms (35.51%), Space: 52.1 MB (38.65%) - LeetHub

---
+++ 1472-design-browser-history/1472-design-browser-history.js
@@ -0,0 +1,66 @@
+/**
+ * @param {string} homepage
+ */
+// var BrowserHistory = function(homepage) {
+    
+// };
+class BrowserHistory{
+    constructor(homepage){
+        this.history=[homepage]
+        this.currIdx=0
+      
+    }
+    visit(url){
+        this.history=this.history.splice(0,this.currIdx+1)
+     
+        this.history.push(url)
+        this.currIdx=this.history.length-1
+        
+        // console.log(this.history,"debug")
+        
+    }
+    back(steps){
+        this.currIdx=Math.max(0,this.currIdx-steps)
+        return this.history[this.currIdx]
+        
+    }
+    forward(steps){
+    
+        this.currIdx=Math.min(this.history.length-1,this.currIdx+steps)
+        
+        return this.history[this.currIdx]
+        
+    }
+}
+
+/** 
+ * @param {string} url
+ * @return {void}
+ */
+// BrowserHistory.prototype.visit = function(url) {
+    
+// };
+
+// /** 
+//  * @param {number} steps
+//  * @return {string}
+//  */
+// BrowserHistory.prototype.back = function(steps) {
+    
+// };
+
+// /** 
+//  * @param {number} steps
+//  * @return {string}
+//  */
+// BrowserHistory.prototype.forward = function(steps) {
+    
+// };
+
+/** 
+ * Your BrowserHistory object will be instantiated and called as such:
+ * var obj = new BrowserHistory(homepage)
+ * obj.visit(url)
+ * var param_2 = obj.back(steps)
+ * var param_3 = obj.forward(steps)
+ */
