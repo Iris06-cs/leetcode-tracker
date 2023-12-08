60c673c4b30c4de2fcb1b26e65bd6a211723439a
Iris Wang <100994372+Iris06-cs@users.noreply.github.com>
2023-12-08 00:51:38 -0800

    Time: 60 ms (84.77%), Space: 46.4 MB (36.23%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -26,36 +26,69 @@
 //     return res
 // };
 
-var topKFrequent = function(nums, k) {
+// var topKFrequent = function(nums, k) {//O(N) time and space
+// //avoid sorting
+// //     first count each number's count in a hash map
+//         let count=new Map()
+//         let res=[]
+//         for(let num of nums){
+//             if(!count.has(num)) count.set(num,0)
+//             count.set(num,count.get(num)+1)
 
-        let count=new Map()
-        let res=[]
-        for(let num of nums){
-            if(!count.has(num)) count.set(num,0)
-            count.set(num,count.get(num)+1)
-
-        }
-        let times=new Map()
-        for(let i=0;i<=nums.length;i++){
-            times.set(i,new Set())
+//         }
+// //     create another hash map with the count as key and map to the numbers with conresponding count
+//         let times=new Map()
+//         for(let i=0;i<=nums.length;i++){
+//             times.set(i,new Set())
             
-        }
-        for(let num of nums){
-            let num_times=count.get(num)
-            times.get(num_times).add(num)
-        }
+//         }
+//         for(let num of nums){
+//             let num_times=count.get(num)
+//             times.get(num_times).add(num)
+//         }
 
-        for(let i=nums.length;i>=0 &&k>0;i--){
-            if(times.get(i).size) {
-                let values=Array.from(times.get(i).values())
-                values.forEach(val=>{
-                res.push(val)
-                k--
-                })
+//         for(let i=nums.length;i>=0 &&k>0;i--){
+//             if(times.get(i).size) {
+//                 let values=Array.from(times.get(i).values())
+//                 values.forEach(val=>{
+//                 res.push(val)
+//                 k--
+//                 })
 
 
-            }
+//             }
+//     }
+
+//     return res
+// };
+
+var topKFrequent = function(nums, k) {
+    let count = new Map(), freq = [], res = [];
+
+    // Count the frequency of each number
+    for (let num of nums) {
+        count.set(num, (count.get(num) || 0) + 1);
     }
 
-    return res
+    // Invert the count map: frequency -> set of numbers
+    for (let [num, freqCount] of count.entries()) {
+        if (!freq[freqCount]) freq[freqCount] = new Set();
+        freq[freqCount].add(num);
+    }
+
+    // Extract top k frequent elements
+    for (let i = freq.length - 1; i >= 0 && k > 0; i--) {
+        if (freq[i]) {
+            for (let num of freq[i]) {
+                if (k > 0) {
+                    res.push(num);
+                    k--;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    return res;
 };
