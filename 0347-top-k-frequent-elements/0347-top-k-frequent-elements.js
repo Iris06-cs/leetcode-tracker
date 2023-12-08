7612502c335401da1ea4f63305514fab5f0ab185
Iris Wang <100994372+Iris06-cs@users.noreply.github.com>
2023-12-08 00:44:26 -0800

    Time: 84 ms (14.19%), Space: 56.3 MB (5.09%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -3,25 +3,59 @@
  * @param {number} k
  * @return {number[]}
  */
-var topKFrequent = function(nums, k) {
-//     hashmap map number and its count
-    let count=new Map()
-    let res=[]
-    for(let num of nums){
-        if(!count.has(num)) count.set(num,0)
-        count.set(num,count.get(num)+1)
+// var topKFrequent = function(nums, k) {
+// //     hashmap map number and its count
+//     let count=new Map()
+//     let res=[]
+//     for(let num of nums){
+//         if(!count.has(num)) count.set(num,0)
+//         count.set(num,count.get(num)+1)
  
-    }
-    const tracker=Array.from(count.entries())
+//     }
+//     const tracker=Array.from(count.entries())
 
-    tracker.sort((a,b)=>{ //nlogn
-        if(a[1]-b[1]>0) return -1
-        if(a[1]-b[1]<0) return 1
-        return 0
-    })
+//     tracker.sort((a,b)=>{ //nlogn
+//         if(a[1]-b[1]>0) return -1
+//         if(a[1]-b[1]<0) return 1
+//         return 0
+//     })
    
-    for(let i=0;i<k;i++){
-        res.push(tracker[i][0])
+//     for(let i=0;i<k;i++){
+//         res.push(tracker[i][0])
+//     }
+//     return res
+// };
+
+var topKFrequent = function(nums, k) {
+// optimize to avoid sorting
+        let count=new Map()
+        let res=[]
+        for(let num of nums){
+            if(!count.has(num)) count.set(num,0)
+            count.set(num,count.get(num)+1)
+
+        }
+        let times=new Map()
+        for(let i=0;i<=nums.length;i++){
+            times.set(i,new Set())
+            
+        }
+        for(let num of nums){
+            let num_times=count.get(num)
+            times.get(num_times).add(num)
+        }
+// console.log(times)
+    for(let i=nums.length;i>=0 &&k>0;i--){
+        if(times.get(i).size) {
+            let values=Array.from(times.get(i).values())
+            values.forEach(val=>{
+                res.push(val)
+                    k--
+            })
+        
+        
+        }
     }
+    // console.log(res)
     return res
 };
