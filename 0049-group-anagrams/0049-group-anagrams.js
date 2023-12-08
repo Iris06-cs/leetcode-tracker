917d6cfbe6119d6d905bb8f6e9c231fa451308de
Iris Wang <100994372+Iris06-cs@users.noreply.github.com>
2023-12-07 23:08:24 -0800

    Time: 159 ms (11.21%), Space: 55 MB (23.77%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+// brute force , sort each word=> so the anagrams will be turned to same word 
+//     time complexity m*nlogn
+    
+//     optimized--hashmap
+    let res={}
+    
+    for(let str of strs){
+        let count=new Array(26).fill(0) // represent a to z
+  
+        for(let i=0;i<str.length;i++){
+            count[str.charCodeAt(i)-97]++  //ascii code of "a" 97
+        }
+        if(!res[count])res[count]=[]
+        // console.log(res)
+        res[count].push(str)
+    }
+    
+    return Object.values(res)
+    
+};
