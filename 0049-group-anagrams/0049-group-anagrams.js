aef1753aee716dd50209e5bc14cfa892d0d38085
Iris Wang <100994372+Iris06-cs@users.noreply.github.com>
2023-12-07 23:32:20 -0800

    Time: 114 ms (45.96%), Space: 55.1 MB (21.76%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -2,12 +2,31 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
-var groupAnagrams = function(strs) {
-// brute force , sort each word=> so the anagrams will be turned to same word 
-//     time complexity m*nlogn
+// var groupAnagrams = function(strs) {
+// // brute force , sort each word=> so the anagrams will be turned to same word 
+// //     time complexity m*nlogn
     
-//     optimized--hashmap
-    let res={}
+// //     optimized--hashmap
+//     let res={}
+    
+//     for(let str of strs){
+//         let count=new Array(26).fill(0) // represent a to z
+  
+//         for(let i=0;i<str.length;i++){
+//             count[str.charCodeAt(i)-97]++  //ascii code of "a" 97
+//         }
+//         if(!res[count])res[count]=[]
+//         // console.log(res)
+//         res[count].push(str)
+//     }
+    
+//     return Object.values(res)
+    
+// };
+
+var groupAnagrams = function(strs) {
+
+    let res=new Map()
     
     for(let str of strs){
         let count=new Array(26).fill(0) // represent a to z
@@ -15,11 +34,13 @@ var groupAnagrams = function(strs) {
         for(let i=0;i<str.length;i++){
             count[str.charCodeAt(i)-97]++  //ascii code of "a" 97
         }
-        if(!res[count])res[count]=[]
+        let key=count.join("#")
+        if(!res.has(key)) res.set(key,[])
+        
+        res.get(key).push(str)
         // console.log(res)
-        res[count].push(str)
     }
     
-    return Object.values(res)
-    
+    return Array.from(res.values())
+   
 };
