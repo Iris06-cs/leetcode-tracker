e27c5dec0f9f4d3d8b7b0c8b12cf0258aa38e04e
Iris Wang <100994372+Iris06-cs@users.noreply.github.com>
2023-12-09 23:18:19 -0800

    Time: 158 ms (45.68%), Space: 71 MB (18.74%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -5,25 +5,49 @@
 var longestConsecutive = function(nums) {
 //     time must be O(n), cannot sort array
 //     brute force sort nums
-        if(!nums.length) return 0
-        nums.sort((a,b)=>a-b)
-        let numSet=new Set(nums)
-    // console.log(numSet)
-        let maxLen=1
-        let subLen=1
-        let values=Array.from(numSet.values())
-        for(let i=1;i<numSet.size;i++){
-            if(values[i]===values[i-1]+1) {
-                subLen++
-                if(subLen>maxLen){
-                    maxLen=subLen
-                }
-            }
+//         if(!nums.length) return 0
+//         nums.sort((a,b)=>a-b)
+//         let numSet=new Set(nums)
+//     // console.log(numSet)
+//         let maxLen=1
+//         let subLen=1
+//         let values=Array.from(numSet.values())
+//         for(let i=1;i<numSet.size;i++){
+//             if(values[i]===values[i-1]+1) {
+//                 subLen++
+//                 if(subLen>maxLen){
+//                     maxLen=subLen
+//                 }
+//             }
             
-            else subLen=1
+//             else subLen=1
 
                            
-        }
-        return maxLen
+//         }
+//         return maxLen
     
+    //without sorting
+    if(!nums.length) return 0
+    
+    let numSet=new Set(nums)
+    let seq={}
+    for(let num of nums){
+        if(!numSet.has(num-1)) seq[num]=1
+    }
+    
+   
+    for(let key in seq){
+        let num=Number(key)+1
+        // console.log(key,num)
+        while(numSet.has(num)){
+            seq[key]++
+            num++
+        }
+    }
+   let maxLen=1
+   for(let val of Object.values(seq)){
+       if(val>maxLen) maxLen=val
+       else continue
+   }
+    return maxLen
 };
