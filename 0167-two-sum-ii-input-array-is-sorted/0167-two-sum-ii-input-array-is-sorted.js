ed48c8878303faf86979c218fe29da0f5ed9fb6c
Iris Wang <100994372+Iris06-cs@users.noreply.github.com>
2023-12-11 23:30:38 -0800

    Time: 61 ms (29.64%), Space: 43 MB (35.82%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -4,14 +4,24 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
-//     brute force
+//     brute force  O(n^2)
 
-    for(let i=0;i<numbers.length-1;i++){
-        const reminder=target-numbers[i]
-        for(let j=i+1;j<numbers.length;j++){
-            if(reminder===numbers[j]) return[i+1,j+1]
-        }
+//     for(let i=0;i<numbers.length-1;i++){
+//         const reminder=target-numbers[i]
+//         for(let j=i+1;j<numbers.length;j++){
+//             if(reminder===numbers[j]) return[i+1,j+1]
+//         }
         
+//     }
+    
+//     two pointer
+    let i=0
+    let j=numbers.length-1
+    
+    while(i<numbers.length && j>=0){
+        if(numbers[i]+numbers[j]>target) j--
+        else if(numbers[i]+numbers[j]===target) return [i+1,j+1]
+        else i++
     }
     
 };
