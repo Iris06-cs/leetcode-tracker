4422a22a0a12eff0eea1730ef2d12734de28f5e4
Iris Wang <100994372+Iris06-cs@users.noreply.github.com>
2023-12-16 01:04:21 -0800

    Time: 362 ms (85.32%), Space: 33.2 MB (83.91%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.py
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.py
@@ -1,23 +1,38 @@
 class Solution:
     def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
-        # hash map to track count of each number in array
-        # remove from the less count number till k = 0
-        count_tracker={}
-        for num in arr:
-            if num not in count_tracker.keys():
-                count_tracker[num]=[num,0]
-            count_tracker[num][1] += 1
-        print(count_tracker)
-        count_arr=count_tracker.values()
-        sorted_arr=sorted(count_arr,key=lambda x:x[1])
-        print(sorted_arr)
-        n=len(sorted_arr)
-        for i in range(n):
-            num,count=sorted_arr[i]
-            if k>0 and count<=k:
-                k=k-count
-                n-=1
+#         # hash map to track count of each number in array
+#         # remove from the less count number till k = 0
+#         count_tracker={}#O(n)
+#         for num in arr:#O(n)
+#             if num not in count_tracker.keys():
+#                 count_tracker[num]=[num,0]
+#             count_tracker[num][1] += 1
+            
+#         count_arr=count_tracker.values()#O(n)
+#         sorted_arr=sorted(count_arr,key=lambda x:x[1])#O(n)  nlogn
+
+#         n=len(sorted_arr)#O(1)
+#         for i in range(n):#O(n)
+#             num,count=sorted_arr[i]
+#             if k>0 and count<=k:
+#                 k=k-count
+#                 n-=1
                 
+#         return n   #overall time O(n+nlogn) space O(n)
+#         use python Counter collection
+        count_tracker=Counter(arr)
+        
+#         sort
+        sorted_counts=sorted(count_tracker.values())
+        n=len(sorted_counts)
+    
+#         remove elements
+        for count in sorted_counts:
+            if k>=count:
+                k-=count
+                n-=1
+            else:
+                break
         return n
             
             
