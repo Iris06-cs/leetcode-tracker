4c217dfa10d7dbbfa37955cf1d2b6e92ef9bb8af
Iris Wang <100994372+Iris06-cs@users.noreply.github.com>
2023-12-17 01:22:52 -0800

    Time: 53 ms (98.59%), Space: 16.5 MB (36.92%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
@@ -1,7 +1,18 @@
 class Solution:
     def sortByBits(self, arr: List[int]) -> List[int]:
-#         change to binary format
-        return sorted(arr,key=lambda x:(bin(x).count("1"),x))
+        # sorted(iterable, key=None, reverse=False)
+        # key: optional function which converts an item to a value to be compared
+        
+#        sort based on tuple(binary_num,num)==>will first sort by binary_num of count 1, if a tie than sort by x
+        # return sorted(arr,key=lambda x:(bin(x).count("1"),x))
+    
+#     without lambda
+        def sorter_logic(num):
+            counting=bin(num).count("1")
+            return counting,num
+        arr.sort(key=sorter_logic)
+        return arr
+        
         
         
         
