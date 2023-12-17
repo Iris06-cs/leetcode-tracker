f741175d7f0f4ef199b094bced586369b1e90f9a
Iris Wang <100994372+Iris06-cs@users.noreply.github.com>
2023-12-17 02:12:45 -0800

    Time: 45 ms (10.63%), Space: 16.3 MB (47.85%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.py
@@ -0,0 +1,12 @@
+class Solution:
+    def breakPalindrome(self, palindrome: str) -> str:
+        if len(palindrome) <=1:
+            return ""
+
+        for idx in range(math.floor(len(palindrome)//2)):
+            char=palindrome[idx]
+            if char != "a":             
+                return palindrome[:idx]+"a"+palindrome[idx+1:]
+#         all letters are a
+        return palindrome[:-1]+"b"
+            
