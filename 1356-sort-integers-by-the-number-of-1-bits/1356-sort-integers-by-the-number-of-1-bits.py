78ccd1360336ceb2df7652ae80460237ca8edb8b
Iris Wang <100994372+Iris06-cs@users.noreply.github.com>
2023-12-17 01:24:45 -0800

    Time: 65 ms (68.31%), Space: 16.4 MB (70.11%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
@@ -5,13 +5,15 @@ class Solution:
         
 #        sort based on tuple(binary_num,num)==>will first sort by binary_num of count 1, if a tie than sort by x
         # return sorted(arr,key=lambda x:(bin(x).count("1"),x))
+        arr.sort(key=lambda x:(bin(x).count("1"),x))
+        return arr
     
 #     without lambda
-        def sorter_logic(num):
-            counting=bin(num).count("1")
-            return counting,num
-        arr.sort(key=sorter_logic)
-        return arr
+        # def sorter_logic(num):
+        #     counting=bin(num).count("1")
+        #     return counting,num
+        # arr.sort(key=sorter_logic)
+        # return arr
         
         
         
