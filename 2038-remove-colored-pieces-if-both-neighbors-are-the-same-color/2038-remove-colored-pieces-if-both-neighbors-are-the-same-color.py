1cf6d773c1db6537c355908d7bf2026d661e12ea
Iris Wang <100994372+Iris06-cs@users.noreply.github.com>
2023-12-18 14:14:53 -0800

    Time: 294 ms (11.32%), Space: 17.3 MB (63.26%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
@@ -2,11 +2,10 @@ class Solution:
     def winnerOfGame(self, colors: str) -> bool:
         sub_strs={"AAA":0,"BBB":0}
         for i in range(len(colors)-2):
-            sub_str=colors[i:i+3]
-            if sub_str=="AAA":
+            if colors[i:i+3]=="AAA":
                 sub_strs["AAA"]+=1
             
-            if sub_str=="BBB":
+            if colors[i:i+3]=="BBB":
                 sub_strs["BBB"]+=1
-        # print(sub_strs)     
+  
         return sub_strs["AAA"]>sub_strs["BBB"]
