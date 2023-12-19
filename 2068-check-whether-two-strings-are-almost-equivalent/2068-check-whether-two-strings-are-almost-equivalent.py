95ca37f6418fb0084e795404b9400fbd2d4b9cba
Iris Wang <100994372+Iris06-cs@users.noreply.github.com>
2023-12-18 23:51:34 -0800

    Time: 35 ms (86.44%), Space: 16.3 MB (56.38%) - LeetHub

--- 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.py
+++ 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.py
@@ -2,13 +2,31 @@ class Solution:
     def checkAlmostEquivalent(self, word1: str, word2: str) -> bool:
 #         track the freq
 #         cal freq diff
-        freqs=Counter(word1)
-        for char in word2:
-            if char in freqs.keys():
-                freqs[char]-=1
-            else:
-                freqs[char]=-1
-        max_diff=max(abs(freq) for freq in freqs.values() )
-        return max_diff<=3
+        # freqs=Counter(word1)
+        # for char in word2:
+        #     if char in freqs.keys():
+        #         freqs[char]-=1
+        #     else:
+        #         freqs[char]=-1
+        # max_diff=max(abs(freq) for freq in freqs.values() )
+        # return max_diff<=3
+        
+        freqs1=Counter(word1)
+        freqs2=Counter(word2)
+        for char,freq in freqs1.items():
+            if char not in freqs2.keys() and freq>3:
+                return False
+            if char in freqs2.keys() and abs(freq-freqs2[char])>3:
+                return False
+            
+        for char,freq in freqs2.items():
+            if char not in freqs1.keys() and freq>3:
+                return False
+            if char in freqs1.keys() and abs(freq-freqs1[char])>3:
+                return False
+        return True
+                
+            
+            
         
                 
