94582227ec339a70705e039124734966e45a3b05
Iris Wang <100994372+Iris06-cs@users.noreply.github.com>
2023-12-18 22:36:45 -0800

    Time: 327 ms (21.75%), Space: 31 MB (51.77%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.py
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.py
@@ -16,14 +16,14 @@ class Solution:
 #         optimize
 #         because the arr will be sorted=>the diff between to nearest two num will be smaller than non-neighbor ones
 #         no need to check all possible pairs
-        arr.sort()
+        arr.sort() #nlogn
         min_diff=math.inf
-        for i in range(len(arr)-1):
+        for i in range(len(arr)-1): #O(n)
             min_diff=min(min_diff,abs(arr[i]-arr[i+1]))
             
-        res=[]
-        for i in range(len(arr)-1):
-            if abs(arr[i]-arr[i+1])==min_diff:
-                res.append([arr[i],arr[i+1]])
-        return res
-        
+        # res=[]
+        # for i in range(len(arr)-1):#O(n)
+        #     if abs(arr[i]-arr[i+1])==min_diff:
+        #         res.append([arr[i],arr[i+1]])
+        # return res
+        return [[arr[i],arr[i+1]] for i in range(len(arr)-1) if abs(arr[i]-arr[i+1])==min_diff]
