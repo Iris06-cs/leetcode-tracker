d3040b1dc781109b23e062312026d8c780a4556d
Iris Wang <100994372+Iris06-cs@users.noreply.github.com>
2023-12-20 02:20:38 -0800

    Time: 56 ms (81.68%), Space: 17.3 MB (12.25%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
@@ -1,18 +1,20 @@
 class Solution:
     def removeAnagrams(self, words: List[str]) -> List[str]:
-        def is_anagrams(str1:List[str],str2:List[str]) -> bool:
-            if len(str1) != len(str2):
-                return False
-            char_counter=Counter(str1)
-            for char in str2:
-                if char in char_counter.keys():
-                    char_counter[char]-=1
-                else:
-                    char_counter[char]=-1
-            for count in char_counter.values():
-                if count != 0:
-                    return False
-            return True
+        # def is_anagrams(str1:List[str],str2:List[str]) -> bool:
+        #     if len(str1) != len(str2):
+        #         return False
+        #     char_counter=Counter(str1)
+        #     for char in str2:
+        #         if char in char_counter.keys():
+        #             char_counter[char]-=1
+        #         else:
+        #             char_counter[char]=-1
+        #     for count in char_counter.values():
+        #         if count != 0:
+        #             return False
+        #     return True
+        def is_anagrams(str1: str, str2: str) -> bool:
+            return sorted(str1) == sorted(str2)
         
         res=[words[0]]
         for i in range(1,len(words)):
