48b7a932e6e73e01eb60307b56c732358b231a38
Iris Wang <100994372+Iris06-cs@users.noreply.github.com>
2023-12-20 02:17:40 -0800

    Time: 73 ms (28.38%), Space: 17.5 MB (12.25%) - LeetHub

---
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
@@ -0,0 +1,24 @@
+class Solution:
+    def removeAnagrams(self, words: List[str]) -> List[str]:
+        def is_anagrams(str1:List[str],str2:List[str]) -> bool:
+            if len(str1) != len(str2):
+                return False
+            char_counter=Counter(str1)
+            for char in str2:
+                if char in char_counter.keys():
+                    char_counter[char]-=1
+                else:
+                    char_counter[char]=-1
+            for count in char_counter.values():
+                if count != 0:
+                    return False
+            return True
+        
+        res=[words[0]]
+        for i in range(1,len(words)):
+            if not is_anagrams(res[-1],words[i]):
+                res.append(words[i])
+        return res
+                
+        
+        
