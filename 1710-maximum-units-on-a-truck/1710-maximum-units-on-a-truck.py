f20f17d9869acb381635d8d4c6c7c5879d0fad74
Iris Wang <100994372+Iris06-cs@users.noreply.github.com>
2023-12-20 03:04:08 -0800

    Time: 133 ms (99.64%), Space: 17.8 MB (8.07%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
@@ -3,10 +3,8 @@ class Solution:
         # sort list by total units, then box number
  
         boxTypes.sort(key=lambda x:x[1],reverse=True)
-        print(boxTypes)
         res=0
         for boxType in boxTypes:
-            print(boxType)
             if boxType[0]<=truckSize:
                 res+=boxType[0]*boxType[1]
                 truckSize-=boxType[0]
