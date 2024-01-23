b4bff2e8592f4347c812c8cd7e40fb347b21a69d
Iris Wang <100994372+Iris06-cs@users.noreply.github.com>
2024-01-23 15:36:53 -0800

    Time: 29 ms (96.37%), Space: 16.5 MB (58.61%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.py
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.py
@@ -2,13 +2,29 @@ class Solution:
     def kthFactor(self, n: int, k: int) -> int:
         # brute force
         # loop each number from 1 to n to find all factors
-        factors = []
-        for num in range(1,n+1):
-            if n % num == 0:
-                factors.append(num)
+#         factors = []
+#         for num in range(1,n+1)://O(n)
+#             if n % num == 0:
+#                 factors.append(num)
                 
-        if len(factors) < k:
-            return -1
-        return factors[k-1]
+#         if len(factors) < k:
+#             return -1
+#         return factors[k-1]
+
+        # less than O(n)
+        # smallest factor is 1 largest factor is n itself
+        # loop when k > 0, if find a factor minus k till k=0
+        num = 1
+        factor = -1
+        while k > 0 and num <= n:
+            if n % num == 0:
+                if k == 1:
+                    factor = num
+                k -= 1
+            num += 1
+        return factor
+        
+        
+                
         
         
