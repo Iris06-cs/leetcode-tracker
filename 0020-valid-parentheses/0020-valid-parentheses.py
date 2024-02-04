67f8e156a1d6323b38d1ecf3bebb1aced478530e
Iris Wang <100994372+Iris06-cs@users.noreply.github.com>
2024-02-03 22:22:51 -0800

    Time: 30 ms (91.54%), Space: 16.5 MB (72.47%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,18 +1,35 @@
 class Solution:
     def isValid(self, s: str) -> bool:
 #         add opening parentheses to stack then pop off when having matching closing parenthese
-        stack=[]
-        openings=["(","[","{"]
-        closings={")":"(","}":"{","]":"["}
+        # stack=[]
+        # openings=["(","[","{"]
+        # closings={")":"(","}":"{","]":"["}
+        # for char in s:
+        #     if char in openings:
+        #         stack.append(char)
+        #     else:
+        #         if stack and closings[char]==stack[-1]:
+        #             stack.pop()
+        #         else:
+        #             return False
+        # return True if not stack else False
+        
+        stack = []
         for char in s:
-            if char in openings:
+            # 压栈
+            if char == "{" or char == "[" or char == "(":
                 stack.append(char)
             else:
-                if stack and closings[char]==stack[-1]:
-                    stack.pop()
-                else:
+                # check if stack is empty
+                if not stack: # empty stack, try to push in closing parentheses
                     return False
-        return True if not stack else False
+                # check if the top of stack will match
+                if char == "]" and stack[-1] != "[" or char == "}" and stack[-1] != "{" or char == ")" and stack[-1] != "(":
+                    return False
+                stack.pop()
+                # whether stack is empty
+        return not stack
+        
         
         
         
