3e78736486c26f94fca29ada01f615a1da1399b0
Iris Wang <100994372+Iris06-cs@users.noreply.github.com>
2024-02-05 00:25:56 -0800

    Time: 113 ms (5.22%), Space: 57.7 MB (5.06%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    // backtracking
+    // add open paranthesis if open<n
+    // add close paranthesis if close<open
+    // one valid way when open=close=n
+    const stack = []
+    const res = []
+    
+    const backtracking=(openNum,closeNum)=>{
+        if(openNum === closeNum && openNum === n){
+            console.log(stack)
+            res.push(stack.join(""))
+        }
+        if(openNum < n){
+            stack.push("(")
+            backtracking(openNum + 1,closeNum)
+            stack.pop()
+        }
+        if(closeNum < openNum){
+            stack.push(")")
+            backtracking(openNum, closeNum + 1)
+            stack.pop()
+        }
+    
+        
+      
+    }   
+    
+    backtracking(0,0)
+      return res  
+};
