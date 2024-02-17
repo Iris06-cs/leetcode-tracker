28b1d3096df3336813d54b3db2cb57fce7ef77bf
Iris Wang <100994372+Iris06-cs@users.noreply.github.com>
2024-02-17 12:02:35 -0800

    Time: 41 ms (86.06%) | Memory: 17.2 MB (54.80%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,29 +1,48 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # change matrix to 1-d array
+        row = len(matrix)
+        if row == 0:
+            return Fasle
+        col = len(matrix[0])
         
-        top = 0
-        down = len(matrix) - 1
-        while top <= down:
-            mid_row = (top + down) // 2
-            mid_row_l = 0
-            mid_row_r = len(matrix[mid_row]) - 1
-            
-            if matrix[mid_row][mid_row_r]<target :
-                # move down
-                top = mid_row + 1
-            elif matrix[mid_row][mid_row_l]>target:
-                # move up
-                down = mid_row - 1
+        l = 0
+        r = row * col - 1
+        while l <= r:
+            mid = (r + l) // 2
+            mid_num = matrix[mid // col][mid % col]
+            if mid_num == target:
+                return True
+            elif mid_num < target:
+                l = mid + 1
             else:
-                while mid_row_l <= mid_row_r:
-                    mid_row_mid_col = (mid_row_l + mid_row_r) // 2
-                    if matrix[mid_row][mid_row_mid_col] == target:
-                        return True
-                    elif matrix[mid_row][mid_row_mid_col] > target:
-                        mid_row_r = mid_row_mid_col - 1
-                    else:
-                        mid_row_l = mid_row_mid_col + 1
-                return False
-            
+                r = mid - 1
         return False
+        
+#         top = 0
+#         down = len(matrix) - 1
+#         while top <= down:
+#             mid_row = (top + down) // 2
+#             mid_row_l = 0
+#             mid_row_r = len(matrix[mid_row]) - 1
+            
+#             if matrix[mid_row][mid_row_r]<target :
+#                 # move down
+#                 top = mid_row + 1
+#             elif matrix[mid_row][mid_row_l]>target:
+#                 # move up
+#                 down = mid_row - 1
+#             else:
+#                 while mid_row_l <= mid_row_r:
+#                     mid_row_mid_col = (mid_row_l + mid_row_r) // 2
+#                     if matrix[mid_row][mid_row_mid_col] == target:
+#                         return True
+#                     elif matrix[mid_row][mid_row_mid_col] > target:
+#                         mid_row_r = mid_row_mid_col - 1
+#                     else:
+#                         mid_row_l = mid_row_mid_col + 1
+#                 return False
+            
+#         return False
+  
         
