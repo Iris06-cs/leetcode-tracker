237d45f13aaa4287220a3dfeaf47cb6d426bfb05
Iris Wang <100994372+Iris06-cs@users.noreply.github.com>
2024-02-26 00:42:15 -0800

    Time: 169 ms (37.66%) | Memory: 27.8 MB (95.30%) - LeetSync

---
+++ 189-rotate-array/rotate-array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # if k same as nums length=> no need to rotate
+        # k%length=> steps need to rotate
+        steps = k % len(nums)
+        # for i in range(steps):#exceeded time limit
+        #     prev = nums[-1]
+        #     for j in range(len(nums)):
+        #         curr = nums[j]
+        #         nums[j] = prev
+        #         prev = curr
+        n = len(nums)
+        temp = nums[n-steps:]+nums[:n-steps]
+        # print(temp)
+        for i in range(len(nums)):
+            nums[i] = temp[i]
