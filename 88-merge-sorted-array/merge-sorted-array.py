cafe0f2b8d41a9033de1df14d17b44230c750cbd
Iris Wang <100994372+Iris06-cs@users.noreply.github.com>
2024-03-03 23:55:27 -0800

    Time: 44 ms (27.68%) | Memory: 16.7 MB (46.13%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -5,7 +5,24 @@ class Solution:
         """
         # merge nums2 into nums1
         # brute force replace elements from m+1 by elements in nums2; then sort nums1
-        for i in range(m,m+n):
-            nums1[i] = nums2[i - m]
+        # for i in range(m,m+n):
+        #     nums1[i] = nums2[i - m]
+        # nums1.sort()  O((n+m)log(n+m))
         
-        nums1.sort()
+        # optimized solution O(n+m)
+        nums1_copy = nums1[:m]
+        p1 = p2 = 0
+        i = 0
+        while i < m+n:
+            # Check if p2 is out of bounds, meaning all elements from nums2 are already inserted
+            if p2 >= n:
+                nums1[i] = nums1_copy[p1]
+                p1 += 1
+            # Check if p1 is out of bounds OR nums1_copy[p1] is greater than nums2[p2]
+            elif p1 >= m or (p2 < n and nums1_copy[p1] > nums2[p2]):
+                nums1[i] = nums2[p2]
+                p2 += 1
+            else:
+                nums1[i] = nums1_copy[p1]
+                p1 += 1
+            i += 1
