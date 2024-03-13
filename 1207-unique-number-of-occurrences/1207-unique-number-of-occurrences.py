c9f7312b208c8f33a6781bb3171b13f6e698ac7f
Iris Wang <100994372+Iris06-cs@users.noreply.github.com>
2024-03-12 23:30:52 -0700

    Time: 42 ms (51.07%), Space: 16.8 MB (74.07%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -1,11 +1,16 @@
 class Solution:
+    # def uniqueOccurrences(self, arr: List[int]) -> bool:
+    #     hashmap = {}
+    #     for num in arr:
+    #         if num not in hashmap:
+    #             hashmap[num] = 0
+    #         hashmap[num] += 1
+    #     freqs = set(hashmap.values())
+    #     return len(freqs) == len(hashmap.values())
+    # use counter collection
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        hashmap = {}
-        for num in arr:
-            if num not in hashmap:
-                hashmap[num] = 0
-            hashmap[num] += 1
-        freqs = set(hashmap.values())
-        return len(freqs) == len(hashmap.values())
+        occurrences = Counter(arr)
+        freqs = occurrences.values()
+        return len(set(freqs)) == len(freqs)
         
         
